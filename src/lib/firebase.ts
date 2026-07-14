f2ca3860de67c8138952e595a4f0222263e80f69
sachloeiws <ipfortaiwan@gmail.com>
2026-07-15 01:21:48 +0800

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -28,7 +28,7 @@ export async function getFirebaseDb() {
     const app = initializeApp(config);
     
     // 初始化 Firestore
-    dbInstance = getFirestore(app, 'Wedding Planner Main');
+    dbInstance = getFirestore(app);
     
     return dbInstance;
   } catch (error) {
